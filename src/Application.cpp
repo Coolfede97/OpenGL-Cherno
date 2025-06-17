4412fdcb986f0d0ca2c961f4583e79ed89afab3e
Coolfede97 <coolfede97@gmail.com>
2025-06-16 21:26:57 -0300

    error handling

--- src/Application.cpp
+++ src/Application.cpp
@@ -1,16 +1,55 @@
 #include <GL/glew.h>
 #include <GLFW/glfw3.h>
 #include <iostream>
+#include <fstream>
+#include <sstream>
+#include <assert.h>
 using namespace std;
+
+#define ASSERT(x) if(!(x)) __debugbreak(); 
+#define GLCall(x) GLClearError();\
+    x; \
+    ASSERT(GLLogCall(#x, __FILE__, __LINE__))
+static void GLClearError()
+{
+    while (glGetError() != GL_NO_ERROR);
+}
+
+static bool GLLogCall(const char* function, const char* file, int line)
+{
+    while (GLenum error = glGetError())
+    {
+        cout << "[OpenGL Error] (" << error << "): " << endl <<
+            "in file: " << file << endl <<
+            "in line: " << line << endl <<
+            "in function: " << function << endl;
+            
+        return false;
+    }
+    return true;
+}
+
+
+string ReadFileToString(const string& filePath)
+{
+    ifstream file(filePath);
+    if (!file.is_open()) cout << "ERROR WHILE TRYING TO OPEN A FILE (ReadFileToString)" << endl;
+    stringstream buffer;
+    buffer << file.rdbuf();
+    return buffer.str();
+}
 
 static unsigned int CompileShader(unsigned int type, const string& source)
 {
     unsigned int id = glCreateShader(type);
-    const char* src = source.c_str();   
+    const char* src = source.c_str();
+    // The second parameter indicates how many strings will be in the array of the source
+    // The second one, if it's nullptr, indicates that the strings of the array will be null terminated
     glShaderSource(id, 1, &src, nullptr);
     glCompileShader(id);
     
     int result;
+    // glGetShaderiv returns a parameter from a shader object. In this case the parameter will be stored in result
     glGetShaderiv(id, GL_COMPILE_STATUS, &result);
     if (result == GL_FALSE)
     {
@@ -33,6 +72,7 @@ static unsigned int CreateShader(const string& vertexShader, const string& fragm
     unsigned int vs = CompileShader(GL_VERTEX_SHADER, vertexShader);
     unsigned int fs = CompileShader(GL_FRAGMENT_SHADER, fragmentShader);
 
+    // A program is a group of glsl that can run on the GPU
     glAttachShader(program, vs);
     glAttachShader(program, fs);
     glLinkProgram(program);
@@ -52,12 +92,20 @@ int main(void)
     if (!glfwInit())
         return -1;
 
-    float positions[6] =
+    float positions[] =
     {
-        -0.5f, -0.5f,
-        0.5f, -0.5f,
-        0, 0.5f
+        -0.5f, -0.5f, // 0 
+        0.5f, -0.5f, // 1
+        0.5f, 0.5f, // 2
+        -0.5f, 0.5f, // 3
     };
+
+    unsigned int indices[] =
+    {
+        0, 1, 2,
+        2, 3, 0
+    };
+
     /* Create a windowed mode window and its OpenGL context */
     window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
     if (!window)
@@ -77,40 +125,31 @@ int main(void)
     unsigned int buffer;
     glGenBuffers(1, &buffer);
     glBindBuffer(GL_ARRAY_BUFFER, buffer);
-    glBufferData(GL_ARRAY_BUFFER, 6 * sizeof(float), positions, GL_STATIC_DRAW);
+    glBufferData(GL_ARRAY_BUFFER, 6 * 2 * sizeof(float), positions, GL_STATIC_DRAW);
     
     glEnableVertexAttribArray(0);
     glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, sizeof(float) * 2, 0);
 
-    string vertexShader =
-        "#version 330 core\n"
-        "\n"
-        "layout(location = 0) in vec4 position;\n"
-        "\n"
-        "void main()\n"
-        "{\n"
-        "gl_Position = position;\n"
-        "}\n";
-    string fragmentShader =
-        "#version 330 core\n"
-        "\n"
-        "layout(location = 0) out vec4 color;\n"
-        "\n"
-        "void main()\n"
-        "{\n"
-        "color = vec4(1.0, 1.0, 0.0, 1);\n"
-        "}\n";
+    unsigned int ibo;
+    glGenBuffers(1, &ibo);
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ibo);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, 6 * sizeof(unsigned int), indices, GL_STATIC_DRAW);
+
+    string vertexShader = ReadFileToString("res/shaders/BasicVertex.glsl");
+    string fragmentShader = ReadFileToString("res/shaders/BasicFragment.glsl");
+
     unsigned int shader = CreateShader(vertexShader, fragmentShader);
     glUseProgram(shader);
+
 
     /* Loop until the user closes the window */
     while (!glfwWindowShouldClose(window))
     {
         /* Render here */
         glClear(GL_COLOR_BUFFER_BIT);
+        
 
-        glDrawArrays(GL_TRIANGLES, 0, 3);
-
+        GLCall(glDrawElements(GL_TRIANGLES, 6, GL_INT, nullptr));
         /* Swap front and back buffers */
         glfwSwapBuffers(window);
 
