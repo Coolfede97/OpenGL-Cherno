7dd94dfdb7bca1d5c48129ae2a8690c731552b54
Coolfede97 <coolfede97@gmail.com>
2025-07-12 20:54:49 -0300

    cubos que giran

--- src/Application.cpp
+++ src/Application.cpp
@@ -1,140 +1,140 @@
-#include <GL/glew.h>
-#include <GLFW/glfw3.h>
-#include <iostream>
-#include <assert.h>
-using namespace std;
-
-#include "Renderer.h"
-
-#include "VertexBuffer.h"
-#include "VertexBufferLayout.h"
-#include "IndexBuffer.h" 
-#include "VertexArray.h"
-#include "Shader.h"
-#include "Texture.h"
-
-#include "glm/glm.hpp"
-#include "glm/gtc/matrix_transform.hpp"
-
-int main(void)
-{
-    GLFWwindow* window;
-
-    /* Initialize the library */
-    if (!glfwInit())
-        return -1;
-
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
-    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
-    {
-
-        float positions[] =
-        {
-          // X      Y     S     T 
-            -0.5f, -0.5f, 0.0f, 0.0f, // 0 
-             0.5f, -0.5f, 1.0f, 0.0f,// 1
-             0.5f,  0.5f, 1.0f, 1.0f,// 2
-            -0.5f,  0.5f, 0.0f, 1.0f// 3
-        };
-
-        unsigned int indices[] =
-        {
-            0, 1, 2,
-            2, 3, 0
-        };
-
-        /* Create a windowed mode window and its OpenGL context */
-        window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
-        if (!window)
-        {
-            glfwTerminate();
-            return -1;
-        }
-
-        /* Make the window's context current */
-        glfwMakeContextCurrent(window);
-
-        glfwSwapInterval(1);
-
-        if (glewInit() != GLEW_OK)
-        {
-            cout << "Error!" << endl;
-        }
-
-		GLCall(glEnable(GL_BLEND));
-		GLCall(glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA));
-
-        int nrAttributes;
-        glGetIntegerv(GL_MAX_VERTEX_ATTRIBS, &nrAttributes);
-        cout << "Max attributes: " << nrAttributes << endl;
-
-        VertexArray va;
-        VertexBuffer vb(positions, 4 * 4 * sizeof(float));
-
-        VertexBufferLayout layout;
-        layout.Push<float>(2);
-        layout.Push<float>(2);
-        va.AddBuffer(vb, layout);
-
-        /*GLCall(glEnableVertexAttribArray(0));
-        GLCall(glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, sizeof(float) * 2, 0));*/
-
-        // index buffer object
-        IndexBuffer ib(indices, 6);
-
-        Shader shader("res/shaders/BasicVertex.glsl", "res/shaders/BasicFragment.glsl");
-        shader.Bind();
-        shader.SetUniform4f("u_Color", 0.7f, 0.1f, 0.4f, 1.0);
-
-        Texture texture("res/textures/francoKO.jpg");
-        texture.Bind();
-        // The second parameter is 0 because that's the slot where we've created the texture 
-        // (that's why the ir no parameter in bind)
-        shader.SetUniform1i("u_Texture", 0);
-
-        va.Unbind();
-        vb.Unbind();
-        ib.Unbind();
-        shader.Unbind();
-
-        Renderer renderer;
-
-        float r = 0.0;
-        float speed = 0.01f;
-        float step = 0.1f;
-
-
-        while (!glfwWindowShouldClose(window))
-        {
-            renderer.Clear();
-
-            shader.Bind();
-            shader.SetUniform4f("u_Color", r, 0.1, 0.4, 1.0);
-			
-            glm::mat4 model = glm::mat4(1.0f);
-            model = glm::rotate(model, (float)glm::radians(-55.0f), glm::vec3(1.0f, 0.0f, 0.0f));
-            shader.SetUniformMat4f("u_model", model);
-
-			glm::mat4 view = glm::mat4(1.0f);
-			view = glm::translate(view, glm::vec3(0.0f, 0.0f, -3.0f));
-			shader.SetUniformMat4f("u_view", view);
-
-                glm::mat4 projection = glm::perspective(glm::radians(45.0f), 800.0f / 600.0f, 0.1f, 100.0f);
-            shader.SetUniformMat4f("u_projection", projection);
-
-            renderer.Draw(va, ib, shader);
-
-            if (r > 1.0f) step = -speed;
-            else if (r < 0.0f) step = speed;
-            r += step;
-
-            glfwSwapBuffers(window);
-
-            glfwPollEvents();
-        }
-
-    }
-    glfwTerminate();
-    return 0;
-}
+//#include <GL/glew.h>
+//#include <GLFW/glfw3.h>
+//#include <iostream>
+//#include <assert.h>
+//using namespace std;
+//
+//#include "Renderer.h"
+//
+//#include "VertexBuffer.h"
+//#include "VertexBufferLayout.h"
+//#include "IndexBuffer.h" 
+//#include "VertexArray.h"
+//#include "Shader.h"
+//#include "Texture.h"
+//
+//#include "glm/glm.hpp"
+//#include "glm/gtc/matrix_transform.hpp"
+//
+//int main(void)
+//{
+//    GLFWwindow* window;
+//
+//    /* Initialize the library */
+//    if (!glfwInit())
+//        return -1;
+//
+//    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+//    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+//    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+//    {
+//
+//        float positions[] =
+//        {
+//          // X      Y     S     T 
+//            -0.5f, -0.5f, 0.0f, 0.0f, // 0 
+//             0.5f, -0.5f, 1.0f, 0.0f,// 1
+//             0.5f,  0.5f, 1.0f, 1.0f,// 2
+//            -0.5f,  0.5f, 0.0f, 1.0f// 3
+//        };
+//
+//        unsigned int indices[] =
+//        {
+//            0, 1, 2,
+//            2, 3, 0
+//        };
+//
+//        /* Create a windowed mode window and its OpenGL context */
+//        window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
+//        if (!window)
+//        {
+//            glfwTerminate();
+//            return -1;
+//        }
+//
+//        /* Make the window's context current */
+//        glfwMakeContextCurrent(window);
+//
+//        glfwSwapInterval(1);
+//
+//        if (glewInit() != GLEW_OK)
+//        {
+//            cout << "Error!" << endl;
+//        }
+//
+//		GLCall(glEnable(GL_BLEND));
+//		GLCall(glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA));
+//
+//        int nrAttributes;
+//        glGetIntegerv(GL_MAX_VERTEX_ATTRIBS, &nrAttributes);
+//        cout << "Max attributes: " << nrAttributes << endl;
+//
+//        VertexArray va;
+//        VertexBuffer vb(positions, 4 * 4 * sizeof(float));
+//
+//        VertexBufferLayout layout;
+//        layout.Push<float>(2);
+//        layout.Push<float>(2);
+//        va.AddBuffer(vb, layout);
+//
+//        /*GLCall(glEnableVertexAttribArray(0));
+//        GLCall(glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, sizeof(float) * 2, 0));*/
+//
+//        // index buffer object
+//        IndexBuffer ib(indices, 6);
+//
+//        Shader shader("res/shaders/BasicVertex.glsl", "res/shaders/BasicFragment.glsl");
+//        shader.Bind();
+//        shader.SetUniform4f("u_Color", 0.7f, 0.1f, 0.4f, 1.0);
+//
+//        Texture texture("res/textures/francoKO.jpg");
+//        texture.Bind();
+//        // The second parameter is 0 because that's the slot where we've created the texture 
+//        // (that's why the ir no parameter in bind)
+//        shader.SetUniform1i("u_Texture", 0);
+//
+//        va.Unbind();
+//        vb.Unbind();
+//        ib.Unbind();
+//        shader.Unbind();
+//
+//        Renderer renderer;
+//
+//        float r = 0.0;
+//        float speed = 0.01f;
+//        float step = 0.1f;
+//
+//
+//        while (!glfwWindowShouldClose(window))
+//        {
+//            renderer.Clear();
+//
+//            shader.Bind();
+//            shader.SetUniform4f("u_Color", r, 0.1, 0.4, 1.0);
+//			
+//            glm::mat4 model = glm::mat4(1.0f);
+//            model = glm::rotate(model, (float)glm::radians(-55.0f), glm::vec3(1.0f, 0.0f, 0.0f));
+//            shader.SetUniformMat4f("u_model", model);
+//
+//			glm::mat4 view = glm::mat4(1.0f);
+//			view = glm::translate(view, glm::vec3(0.0f, 0.0f, -3.0f));
+//			shader.SetUniformMat4f("u_view", view);
+//
+//                glm::mat4 projection = glm::perspective(glm::radians(45.0f), 800.0f / 600.0f, 0.1f, 100.0f);
+//            shader.SetUniformMat4f("u_projection", projection);
+//
+//            renderer.Draw(va, ib, shader);
+//
+//            if (r > 1.0f) step = -speed;
+//            else if (r < 0.0f) step = speed;
+//            r += step;
+//
+//            glfwSwapBuffers(window);
+//
+//            glfwPollEvents();
+//        }
+//
+//    }
+//    glfwTerminate();
+//    return 0;
+//}
--- src/Test.cpp
+++ src/Test.cpp
@@ -1,169 +1,184 @@
-//#include <GL/glew.h>
-//#include <GLFW/glfw3.h>
-//#include <iostream>
-//#include <fstream>
-//#include <sstream>
-//#include <assert.h>
-//using namespace std;
-//
-//#include "Renderer.h"
-//
-//#include "VertexBuffer.h"
-//#include "IndexBuffer.h" 
-//#include "VertexArray.h"
-//
-//string ReadFileToString(const string& filePath)
-//{
-//    ifstream file(filePath);
-//    if (!file.is_open()) cout << "ERROR WHILE TRYING TO OPEN A FILE (ReadFileToString)" << endl;
-//    stringstream buffer;
-//    buffer << file.rdbuf();
-//    return buffer.str();
-//}
-//
-//static unsigned int CompileShader(unsigned int type, const string& source)
-//{
-//    unsigned int id = glCreateShader(type);
-//    const char* src = source.c_str();
-//    // The second parameter indicates how many strings will be in the array of the source
-//    // The second one, if it's nullptr, indicates that the strings of the array will be null terminated
-//    GLCall(glShaderSource(id, 1, &src, nullptr));
-//    GLCall(glCompileShader(id));
-//
-//    int result;
-//    // glGetShaderiv returns a parameter from a shader object. In this case the parameter will be stored in result
-//    glGetShaderiv(id, GL_COMPILE_STATUS, &result);
-//    if (result == GL_FALSE)
-//    {
-//        int length;
-//        GLCall(glGetShaderiv(id, GL_INFO_LOG_LENGTH, &length));
-//        char* message = (char*)alloca(length * sizeof(char));
-//        GLCall(glGetShaderInfoLog(id, length, &length, message));
-//        cout << "Failed to compile " << (type == GL_VERTEX_SHADER ? "vertex" : "fragment") << "shader! " << endl;
-//        cout << message << endl;
-//        GLCall(glDeleteShader(id));
-//        return 0;
-//    }
-//
-//    return id;
-//}
-//
-//static unsigned int CreateShader(const string& vertexShader, const string& fragmentShader)
-//{
-//    unsigned int program = glCreateProgram();
-//    unsigned int vs = CompileShader(GL_VERTEX_SHADER, vertexShader);
-//    unsigned int fs = CompileShader(GL_FRAGMENT_SHADER, fragmentShader);
-//
-//    // A program is a group of glsl that can run on the GPU
-//    GLCall(glAttachShader(program, vs));
-//    GLCall(glAttachShader(program, fs));
-//    GLCall(glLinkProgram(program));
-//    GLCall(glValidateProgram(program));
-//
-//    GLCall(glDeleteShader(vs));
-//    GLCall(glDeleteShader(fs));
-//
-//    return program;
-//}
-//
-//int main(void)
-//{
-//    GLFWwindow* window;
-//
-//    /* Initialize the library */
-//    if (!glfwInit())
-//        return -1;
-//
-//    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-//    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
-//    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
-//    {
-//        float positions[] =
-//        {
-//            -0.5f, -0.5f, // 0 
-//            0.5f, -0.5f, // 1
-//            0.5f, 0.5f, // 2
-//            -0.5f, 0.5f, // 3
-//        };
-//
-//        window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
-//        /* Create a windowed mode window and its OpenGL context */
-//        if (!window)
-//        {
-//            glfwTerminate();
-//            return -1;
-//        }
-//
-//        /* Make the window's context current */
-//        glfwMakeContextCurrent(window);
-//
-//        glfwSwapInterval(1);
-//
-//        if (glewInit() != GLEW_OK)
-//        {
-//            cout << "Error!" << endl;
-//        }
-//        vector<float> xySurface;
-//
-//        float vertexCount = 60.0;
-//        float vertexStep = 2.0 / (vertexCount - 1.0);
-//
-//        for (int row = 0.0; row < vertexCount; ++row)
-//        {
-//            for (int column = 0.0; column < vertexCount; ++column)
-//            {
-//                xySurface.push_back(-1.0+ column * vertexStep);
-//                xySurface.push_back(-1.0+ row * vertexStep);
-//            }
-//        }
-//
-//        float* surface = new float[xySurface.size()];
-//        copy(xySurface.begin(), xySurface.end(), surface);
-//
-//        VertexArray va;
-//        VertexBuffer vb(surface, xySurface.size()*sizeof(float));
-//
-//        VertexBufferLayout layout;
-//        layout.Push<float>(2);
-//        va.AddBuffer(vb, layout);
-//
-//
-//        string vertexShader = ReadFileToString("res/shaders/BasicVertex.glsl");
-//        string fragmentShader = ReadFileToString("res/shaders/BasicFragment.glsl");
-//
-//        unsigned int shader = CreateShader(vertexShader, fragmentShader);
-//        GLCall(glUseProgram(shader));
-//
-//        GLCall(int location = glGetUniformLocation(shader, "u_Color"));
-//        ASSERT(location != -1);
-//        GLCall(glUniform4f(location, 0.7f, 0.1f, 0.4f, 1.0));
-//
-//        GLCall(glBindVertexArray(0));
-//        GLCall(glBindBuffer(GL_ARRAY_BUFFER, 0));
-//        GLCall(glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0));
-//        GLCall(glUseProgram(0));
-//
-//        float r = 0;
-//        float speed = 0.01f;
-//        float step = 0.1f;
-//
-//        while (!glfwWindowShouldClose(window))
-//        {
-//            glClear(GL_COLOR_BUFFER_BIT);
-//
-//            GLCall(glUseProgram(shader));
-//
-//            va.Bind();
-//
-//            GLCall(glDrawArrays(GL_POINTS, 0, xySurface.size()/2));
-//
-//            glfwSwapBuffers(window);
-//
-//            glfwPollEvents();
-//        }
-//
-//        glDeleteProgram(shader);
-//    }
-//    glfwTerminate();
-//    return 0;
-//}
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
+#include <iostream>
+#include <assert.h>
+using namespace std;
+
+#include "Renderer.h"
+
+#include "VertexBuffer.h"
+#include "VertexBufferLayout.h"
+#include "IndexBuffer.h" 
+#include "VertexArray.h"
+#include "Shader.h"
+#include "Texture.h"
+
+#include "glm/glm.hpp"
+#include "glm/gtc/matrix_transform.hpp"
+
+int main(void)
+{
+    GLFWwindow* window;
+
+    /* Initialize the library */
+    if (!glfwInit())
+        return -1;
+
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    {
+
+        /* Create a windowed mode window and its OpenGL context */
+        window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
+        if (!window)
+        {
+            glfwTerminate();
+            return -1;
+        }
+
+        /* Make the window's context current */
+        glfwMakeContextCurrent(window);
+
+        glfwSwapInterval(1);
+
+        if (glewInit() != GLEW_OK)
+        {
+            cout << "Error!" << endl;
+        }
+
+        glEnable(GL_DEPTH_TEST);
+
+        GLCall(glEnable(GL_BLEND));
+        GLCall(glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA));
+
+		float vertices[] = {
+	-0.5f, -0.5f, -0.5f,  0.0f, 0.0f,
+	 0.5f, -0.5f, -0.5f,  1.0f, 0.0f,
+	 0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+	 0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+	-0.5f,  0.5f, -0.5f,  0.0f, 1.0f,
+	-0.5f, -0.5f, -0.5f,  0.0f, 0.0f,
+
+	-0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+	 0.5f, -0.5f,  0.5f,  1.0f, 0.0f,
+	 0.5f,  0.5f,  0.5f,  1.0f, 1.0f,
+	 0.5f,  0.5f,  0.5f,  1.0f, 1.0f,
+	-0.5f,  0.5f,  0.5f,  0.0f, 1.0f,
+	-0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+
+	-0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+	-0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+	-0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+	-0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+	-0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+	-0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+
+	 0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+	 0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+	 0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+	 0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+	 0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+	 0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+
+	-0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+	 0.5f, -0.5f, -0.5f,  1.0f, 1.0f,
+	 0.5f, -0.5f,  0.5f,  1.0f, 0.0f,
+	 0.5f, -0.5f,  0.5f,  1.0f, 0.0f,
+	-0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+	-0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+
+	-0.5f,  0.5f, -0.5f,  0.0f, 1.0f,
+	 0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+	 0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+	 0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+	-0.5f,  0.5f,  0.5f,  0.0f, 0.0f,
+	-0.5f,  0.5f, -0.5f,  0.0f, 1.0f
+		};
+
+		glm::vec3 cubePositions[] = {
+	glm::vec3(0.0f,  0.0f,  0.0f),
+	glm::vec3(2.0f,  5.0f, -15.0f),
+	glm::vec3(-1.5f, -2.2f, -2.5f),
+	glm::vec3(-3.8f, -2.0f, -12.3f),
+	glm::vec3(2.4f, -0.4f, -3.5f),
+	glm::vec3(-1.7f,  3.0f, -7.5f),
+	glm::vec3(1.3f, -2.0f, -2.5f),
+	glm::vec3(1.5f,  2.0f, -2.5f),
+	glm::vec3(1.5f,  0.2f, -1.5f),
+	glm::vec3(-1.3f,  1.0f, -1.5f),
+	glm::vec3(0.0f,   0.0f,   0.0f),
+	glm::vec3(-2.0f,  -5.0f,  15.0f),
+	glm::vec3(1.5f,   2.2f,   2.5f),
+	glm::vec3(3.8f,   2.0f,  12.3f),
+	glm::vec3(-2.4f,   0.4f,   3.5f),
+	glm::vec3(1.7f,  -3.0f,   7.5f),
+	glm::vec3(-1.3f,   2.0f,   2.5f),
+	glm::vec3(-1.5f,  -2.0f,   2.5f),
+	glm::vec3(-1.5f,  -0.2f,   1.5f),
+	glm::vec3(1.3f,  -1.0f,   1.5f)
+
+		};
+
+        VertexArray va;
+        VertexBuffer vb(vertices, 5 * 36 * sizeof(float));
+        VertexBufferLayout layout;
+        layout.Push<float>(3);
+        layout.Push<float>(2);
+
+        va.AddBuffer(vb, layout);
+
+
+        Shader shader("res/shaders/BasicVertex.glsl", "res/shaders/BasicFragment.glsl");
+        shader.Bind();
+
+        Texture texture("res/textures/francoKO.jpg");
+        texture.Bind();
+        // The second parameter is 0 because that's the slot where we've created the texture 
+        // (that's why the ir no parameter in bind)
+        shader.SetUniform1i("u_Texture", 0);
+
+        shader.Unbind();
+
+		float r = 0;
+		float g = 0;
+		float b = 0;
+		float speed  = 10;
+        while (!glfwWindowShouldClose(window))
+        {
+			r = (sin(glfwGetTime() * 10.0f) + 1.0f) / 2.0f;
+			g = (sin(glfwGetTime() * 4.0f + 2.0f) + 1.0f) / 2.0f;
+			b = (sin(glfwGetTime() * 4.0f + 4.0f) + 1.0f) / 2.0f;
+
+			glClearColor(r, g, b, 1.0f);
+			glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+            shader.Bind();
+
+
+			glm::mat4 view = glm::mat4(1.0f);
+			view = glm::translate(view, glm::vec3(0.0f, 0.0f, -3.0f));
+			shader.SetUniformMat4f("u_view", view);
+
+			glm::mat4 projection = glm::perspective(glm::radians(45.0f), 800.0f / 600.0f, 0.1f, 100.0f);
+			shader.SetUniformMat4f("u_projection", projection);
+
+            for (int i = 0; i < sizeof(cubePositions)/sizeof(cubePositions[0]); i++)
+            {
+				glm::mat4 model = glm::mat4(1.0f);
+				model = glm::rotate(model, (float)glfwGetTime(), glm::vec3(1.0f, 1.0f, 1.0f));
+				model = glm::translate(model, cubePositions[i]);
+				shader.SetUniformMat4f("u_model", model);
+				va.Bind();
+				glDrawArrays(GL_TRIANGLES, 0, 36);
+            }
+
+            glfwSwapBuffers(window);
+
+            glfwPollEvents();
+        }
+
+    }
+    glfwTerminate();
+    return 0;
+}
